64ded6f4c03556f017dd39239d26b1eb617bd18e
AdrienDmt <adrien.dumont19@gmail.com>
2017-04-06 11:14:06 +0200

    Classe_php

---
+++ ppe4 php/experience.php
@@ -0,0 +1,85 @@
+<?php
+public class experience {
+	private int id_exp;
+	private String entreprise;
+	private String ville_exp;
+	private String pays_exp;
+	private Date date_debut;
+	private Date date_fin;
+	private String description;
+	
+	//Getter & Setter ID
+	public int getId_exp() {
+		return id_exp;
+	}
+	public void setId_exp(int id_exp) {
+		this.id_exp = id_exp;
+	}
+	
+	//Getter & Setter Entreprise
+	public String getEntreprise() {
+		return entreprise;
+	}
+	public void setEntreprise(String entreprise) {
+		this.entreprise = entreprise;
+	}
+	
+	//Getter & Setter Ville
+	public String getVille_exp() {
+		return ville_exp;
+	}
+	public void setVille_exp(String ville_exp) {
+		this.ville_exp = ville_exp;
+	}
+	
+	//Getter & Setter Pays
+	public String getPays_exp() {
+		return pays_exp;
+	}
+	public void setPays_exp(String pays_exp) {
+		this.pays_exp = pays_exp;
+	}
+	
+	//Getter & Setter Date d�but
+	public Date getDate_debut() {
+		return date_debut;
+	}
+	public void setDate_debut(Date date_debut) {
+		this.date_debut = date_debut;
+	}
+	
+	//Getter & Setter Date fin
+	public Date getDate_fin() {
+		return date_fin;
+	}
+	public void setDate_fin(Date date_fin) {
+		this.date_fin = date_fin;
+	}
+	
+	//Getter & Setter Description
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	//SQL
+	public void affich_exp(){
+		String affich_exp = "SELECT * FROM experience";
+	}
+	
+	public void suppr_exp(){
+		String suppr_exp = "DELETE FROM experience";
+	}
+	
+	public void update_exp(){
+		String update_exp = "UPDATE experience SET entreprise = '', ville_exp = '', pays_exp = '', date_debut = '', date_fin = '', description = '' ";
+	}
+	
+	public void ajout_exp(){
+		String ajout_exp = "INSERT INTO experience VALUES (0, '', '', '', '', '', '')";
+	}
+}
+
+?>
---
+++ ppe4 php/type_interet.php
@@ -0,0 +1,26 @@
+<?php
+public class type_interet {
+	private int id_type_int;
+	private String lib_type_int;
+	
+	//Getter & Setter ID
+	public int getId_type_int() {
+		return id_type_int;
+	}
+	public void setId_type_int(int id_type_int) {
+		this.id_type_int = id_type_int;
+	}
+	
+	//Getter & Setter Libell�
+	public String getLib_type_int() {
+		return lib_type_int;
+	}
+		public class type_interet {
+		this.lib_type_int = lib_type_int;
+	}
+	
+	//SQL
+	public void affich_type_int(){
+		String affich_type_int = "SELECT * FROM type_interet";
+	}
+?>
